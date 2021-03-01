a33be6348d39c8c662e5a50886748bc844db0f3f
Joona Piirainen <61139818+japiirainen@users.noreply.github.com>
2021-03-01 20:46:36 +0200

    Delete dist directory

--- dist/features/repo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAvailabilities = exports.getCategory = void 0;
-const fetch_1 = require("../infrastructure/fetch");
-const getCategory = c => fetch_1.teFetch(baseUrl => fetch_1.retryFetch(`${baseUrl}/products/${c}`, 20));
-exports.getCategory = getCategory;
-const getAvailabilities = m => fetch_1.teFetch(baseUrl => fetch_1.retryFetch(`${baseUrl}/availability/${m}`, 20));
-exports.getAvailabilities = getAvailabilities;
-//# sourceMappingURL=repo.js.map
